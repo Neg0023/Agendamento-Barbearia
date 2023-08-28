d8f556bdf6c02f163941e945ed8f9c36cd97bb06
Uemerson <uemerson161@hotmail.com>
2023-08-28 19:59:28 -0300

    Entidade Customer criada

---
+++ src/main/java/com/barbearia/agendamento/model/Costumer.java
@@ -0,0 +1,22 @@
+package com.barbearia.agendamento.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "customer")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+public class Costumer {
+
+    @Id
+    private String id;
+    private String name;
+    private String cpf;
+    private String phoneNumber;
+}
