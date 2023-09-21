75be7751e973d48abc7e519f989044287123683f
Uemerson <uemerson161@hotmail.com>
2023-09-21 20:31:21 -0300

    ServiÃco de autenticaÃcao criado e mudanca no processo de seguranca de stateful para stateless

--- src/main/java/com/barbearia/agendamento/repository/UserRepository.java
+++ src/main/java/com/barbearia/agendamento/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.barbearia.agendamento.repository;
 
 import com.barbearia.agendamento.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    UserDetails findByEmail(String username);
 }
